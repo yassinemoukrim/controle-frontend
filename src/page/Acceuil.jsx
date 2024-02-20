7c9864389c70a41e44823bd611591604da426666
yassinemoukrim <ymoikrime@gmail.com>
2024-02-20 14:30:40 +0100

    first commit

---
+++ src/page/Acceuil.jsx
@@ -0,0 +1,6 @@
+function Acceuil(){
+    return (
+        <div>Acceuil</div>
+    )
+}
+export default Acceuil
---
+++ src/page/Erreur.jsx
@@ -0,0 +1,6 @@
+function Erreur(){
+    return (
+        <div>Erreur</div>
+    )
+}
+export default Erreur
